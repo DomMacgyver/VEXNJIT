20c67baa246e7ce4370be1300595af0adbb291bc
DaleNaci <dale.nash01@gmail.com>
2019-12-04 13:43:28 -0500

    Created untested drive code

--- PROSBigBot/src/main.cpp
+++ PROSBigBot/src/main.cpp
@@ -32,6 +32,15 @@ Motor trayMotor(TILTER_PORT);
 Motor leftLift(LEFT_LIFT_PORT);
 Motor rightLift(RIGHT_LIFT_PORT);
 
+Motor leftDrive1(LEFT_DRIVE_1_PORT);
+Motor leftDrive2(LEFT_DRIVE_2_PORT);
+Motor leftDrive3(LEFT_DRIVE_3_PORT);
+Motor leftDrive4(LEFT_DRIVE_4_PORT);
+Motor rightDrive1(RIGHT_DRIVE_1_PORT);
+Motor rightDrive2(RIGHT_DRIVE_2_PORT);
+Motor rightDrive3(RIGHT_DRIVE_3_PORT);
+Motor rightDrive4(RIGHT_DRIVE_4_PORT);
+
 
 void on_center_button() {}
 void disabled() {}
@@ -52,6 +61,17 @@ void initialize() {
 	rightLift.setBrakeMode(AbstractMotor::brakeMode::hold);
 	leftLift.setGearing(AbstractMotor::gearset::green);
 	rightLift.setGearing(AbstractMotor::gearset::green);
+
+	rightDrive1.setGearing(AbstractMotor::gearset::green);
+	rightDrive2.setGearing(AbstractMotor::gearset::green);
+	rightDrive3.setGearing(AbstractMotor::gearset::green);
+	rightDrive4.setGearing(AbstractMotor::gearset::green);
+	leftDrive1.setGearing(AbstractMotor::gearset::green);
+	leftDrive2.setGearing(AbstractMotor::gearset::green);
+	leftDrive3.setGearing(AbstractMotor::gearset::green);
+	leftDrive4.setGearing(AbstractMotor::gearset::green);
+
+
 }
 
 
@@ -131,6 +151,28 @@ void liftControl(ControllerButton upBtn, ControllerButton downBtn) {
 	}
 }
 
+/**
+ * Moves the drivetrain based on the "arcade" controls. There is no
+ * priority, the two values are just added together. For example, if
+ * input movements to move mostly forward and slightly to the right,
+ * the left side of the drive will move slightly faster than the right
+ * side.
+ */
+void driveControl() {
+	double leftY = masterController.getAnalog(ControllerAnalog::leftY);
+	double rightX = masterContrlller.getAnalog(ControllerAnalog::rightX);
+	double val1 = pow((leftY * 10), 2) * 2;
+	double val2 = pow((rightX * 10), 2) * 2;
+
+	rightDrive1.move_velocity(val1 - val2);
+	rightDrive2.move_velocity(val2 - val1);
+	rightDrive3.move_velocity(val2 - val1);
+	rightDrive4.move_velocity(val1 - val2);
+	leftDrive1.move_velocity(val1 + val2);
+	leftDrive2.move_velocity(-val1 - val2);
+	leftDrive3.move_velocity(-val1 - val2);
+	leftDrive4.move_velocity(val1 + val2);
+}
 
 void opcontrol() {
 
@@ -138,6 +180,7 @@ void opcontrol() {
 		rollerControl(intakeIn, intakeOut);
 		tilterControl(trayUp, trayDown);
 		liftControl(liftUp, liftDown);
+		driveControl();
 
 		pros::delay(20);
 	}
