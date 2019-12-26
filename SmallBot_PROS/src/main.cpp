6e540d4a12358277c676cc6b14361892a7a71d02
DaleNacianceno <dale.nash01@gmail.com>
2019-12-26 12:23:51 -0500

    Added untested clamp code

--- SmallBot_PROS/src/main.cpp
+++ SmallBot_PROS/src/main.cpp
@@ -6,8 +6,16 @@ int LEFT_FRONT_DRIVE_PORT = 20;
 int LEFT_BACK_DRIVE_PORT = 10;
 int RIGHT_FRONT_DRIVE_PORT = 11;
 int RIGHT_BACK_DRIVE_PORT = 1;
+int LEFT_CLAMP_PORT = 19;
+int RIGHT_CLAMP_PORT = 12;
 
 Controller joystick;
+
+Motor leftClampMotor(LEFT_CLAMP_PORT, false, AbstractMotor::gearset::red);
+Motor rightClampMotor(RIGHT_CLAMP_PORT, false, AbstractMotor::gearset::red);
+
+ControllerButton clampBtn(ControllerDigital::L1);
+
 
 auto chassis = ChassisModelFactory::create(
 	LEFT_FRONT_DRIVE_PORT,
@@ -18,21 +26,59 @@ auto chassis = ChassisModelFactory::create(
 );
 
 
+
 void on_center_button() {}
-void initialize() {}
+
+
+
+void initialize() {
+	leftClampMotor.tarePosition();
+	rightClampMotor.tarePosition();
+}
+
+
+
 void disabled() {}
 void competition_initialize() {}
 void autonomous() {}
 
 
+/**
+ * Moves the clamp to a specific absolute position. Position will
+ * depend on the pos parameter. Speed will depend on the speed
+ * parameter.
+*/
+void clampPosition(int pos, int speed) {
+	leftClampMotor.move_absolute(pos, speed);
+	rightClampMotor.move_absolute(-pos, speed);
+}
+
+
+/**
+ * Moves the clamp in our out, depending on when the L1 button is
+ * pressed. If the clamp is already inwards, pressing it will move it
+ * out. If it is already outwards, pressing it will move it in.
+*/
+void clampControl() {
+	if (clampBtn.changedToPressed()) {
+		if (leftClampMotor.get_position() < 20) {
+			clampPosition(300, 10);
+		} else if (leftClampMotor.get_position() > 280) {
+			clampPosition(0, 10);
+		}
+	}
+}
+
 
 void opcontrol() {
 	while (true) {
 		chassis.xArcade(
-			0,
+			joystick.getAnalog(ControllerAnalog::leftX) * 0.9,
 			joystick.getAnalog(ControllerAnalog::leftY),
 			joystick.getAnalog(ControllerAnalog::rightX) * 0.7
 		);
+
+		clampControl();
 
 		pros::delay(20);
 	}
