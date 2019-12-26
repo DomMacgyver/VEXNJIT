01b02c5f55891b269065695a063311f4812849f2
DaleNacianceno <dale.nash01@gmail.com>
2019-12-26 11:50:55 -0500

    Added untested drive code

--- SmallBot_PROS/src/main.cpp
+++ SmallBot_PROS/src/main.cpp
@@ -1,5 +1,21 @@
 #include "main.h"
 
+using namespace std;
+
+int LEFT_FRONT_DRIVE_PORT = 20;
+int LEFT_BACK_DRIVE_PORT = 10;
+int RIGHT_FRONT_DRIVE_PORT = 11;
+int RIGHT_BACK_DRIVE_PORT = 1;
+
+Controller joystick;
+
+auto chassis = ChassisModelFactory::create(
+	LEFT_FRONT_DRIVE_PORT,
+	RIGHT_FRONT_DRIVE_PORT,
+	RIGHT_BACK_DRIVE_PORT,
+	LEFT_BACK_DRIVE_PORT,
+	200.0
+);
 
 
 void on_center_button() {}
@@ -12,6 +28,11 @@ void autonomous() {}
 
 void opcontrol() {
 	while (true) {
+		chassis.xArcade(
+			0,
+			joystick.getAnalog(ControllerAnalog::leftY),
+			joystick.getAnalog(ControllerAnalog::rightX) * 0.7
+		);
 
 		pros::delay(20);
 	}
