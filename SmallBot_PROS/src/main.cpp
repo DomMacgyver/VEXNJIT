52e3a0aad5a2640619d397334e08e3c028c1e3e9
DaleNaci <dale.nash01@gmail.com>
2020-01-05 11:25:52 -0500

    Updated Small Bot code

--- SmallBot_PROS/src/main.cpp
+++ SmallBot_PROS/src/main.cpp
@@ -8,10 +8,10 @@ int RIGHT_FRONT_DRIVE_PORT = 11;
 int RIGHT_BACK_DRIVE_PORT = 1;
 int LEFT_CLAMP_PORT = 19;
 int RIGHT_CLAMP_PORT = 12;
-int TOP_LEFT_LIFT_PORT = 2;
-int BOTTOM_LEFT_LIFT_PORT = 3;
-int TOP_RIGHT_LIFT_PORT = 9;
-int BOTTOM_RIGHT_LIFT_PORT = 8;
+int TOP_LEFT_LIFT_PORT = 9;
+int BOTTOM_LEFT_LIFT_PORT = 8;
+int TOP_RIGHT_LIFT_PORT = 2;
+int BOTTOM_RIGHT_LIFT_PORT = 3;
 
 Controller joystick;
 
@@ -21,15 +21,27 @@ Motor topLeftLiftMotor(TOP_LEFT_LIFT_PORT, false, AbstractMotor::gearset::red);
 Motor bottomLeftLiftMotor(BOTTOM_LEFT_LIFT_PORT, false, AbstractMotor::gearset::red);
 Motor topRightLiftMotor(TOP_RIGHT_LIFT_PORT, false, AbstractMotor::gearset::red);
 Motor bottomRightLiftMotor(BOTTOM_RIGHT_LIFT_PORT, false, AbstractMotor::gearset::red);
+Motor leftFrontDriveMotor(LEFT_FRONT_DRIVE_PORT, false, AbstractMotor::gearset::green);
+Motor leftBackDriveMotor(LEFT_BACK_DRIVE_PORT, false, AbstractMotor::gearset::green);
+Motor rightFrontDriveMotor(RIGHT_FRONT_DRIVE_PORT, true, AbstractMotor::gearset::green);
+Motor rightFrontBackMotor(RIGHT_BACK_DRIVE_PORT, true, AbstractMotor::gearset::green);
 
-ControllerButton clampBtn(ControllerDigital::L1);
+
+ControllerButton clampInBtn(ControllerDigital::L1);
+ControllerButton clampOutBtn(ControllerDigital::L2);
+ControllerButton liftUpBtn(ControllerDigital::R1);
+ControllerButton liftDownBtn(ControllerDigital::R2);
+
+ControllerButton presetX(ControllerDigital::X);
+ControllerButton presetA(ControllerDigital::A);
+ControllerButton presetB(ControllerDigital::B);
 
 
 auto chassis = ChassisModelFactory::create(
-	LEFT_FRONT_DRIVE_PORT,
-	RIGHT_FRONT_DRIVE_PORT,
-	RIGHT_BACK_DRIVE_PORT,
-	LEFT_BACK_DRIVE_PORT,
+	leftFrontDriveMotor,
+	rightFrontDriveMotor,
+	rightFrontBackMotor,
+	leftBackDriveMotor,
 	200.0
 );
 
@@ -42,6 +54,18 @@ void on_center_button() {}
 void initialize() {
 	leftClampMotor.tarePosition();
 	rightClampMotor.tarePosition();
+	topLeftLiftMotor.tarePosition();
+
+	topLeftLiftMotor.setBrakeMode(AbstractMotor::brakeMode::hold);
+	topRightLiftMotor.setBrakeMode(AbstractMotor::brakeMode::hold);
+	bottomLeftLiftMotor.setBrakeMode(AbstractMotor::brakeMode::hold);
+	bottomRightLiftMotor.setBrakeMode(AbstractMotor::brakeMode::hold);
+
+	leftClampMotor.setBrakeMode(AbstractMotor::brakeMode::hold);
+	rightClampMotor.setBrakeMode(AbstractMotor::brakeMode::hold);
+
+	pros::lcd::initialize();
+
 }
 
 
@@ -49,6 +73,12 @@ void initialize() {
 void disabled() {}
 void competition_initialize() {}
 void autonomous() {}
+
+
+void clamp(int speed) {
+	leftClampMotor.move_velocity(speed);
+	rightClampMotor.move_velocity(-speed);
+}
 
 
 /**
@@ -68,12 +98,54 @@ void clampPosition(int pos, int speed) {
  * out. If it is already outwards, pressing it will move it in.
 */
 void clampControl() {
-	if (clampBtn.changedToPressed()) {
-		if (leftClampMotor.get_position() < 20) {
-			clampPosition(300, 10);
-		} else if (leftClampMotor.get_position() > 280) {
-			clampPosition(0, 10);
-		}
+	if (clampInBtn.isPressed()) {
+		clamp(60);
+	} else if (clampOutBtn.isPressed()) {
+		clamp(-60);
+	} else {
+		clamp(0);
+	}
+}
+
+
+void lift(int speed) {
+	topLeftLiftMotor.move_velocity(-speed);
+	topRightLiftMotor.move_velocity(speed);
+	bottomLeftLiftMotor.move_velocity(speed);
+	bottomRightLiftMotor.move_velocity(-speed);
+}
+
+
+void liftPosition(int pos, int speed) {
+	topLeftLiftMotor.move_absolute(-pos, speed);
+	bottomLeftLiftMotor.move_absolute(pos, speed);
+	topRightLiftMotor.move_absolute(pos, speed);
+	bottomRightLiftMotor.move_absolute(-pos, speed);
+}
+
+
+void liftControl() {
+	if (liftUpBtn.isPressed()) {
+		lift(80);
+	} else if (liftDownBtn.isPressed() && topLeftLiftMotor.get_position() < -30) {
+		lift(-50);
+	}
+
+	if (liftUpBtn.changedToReleased() || liftDownBtn.changedToReleased()) {
+		lift(0);
+	}
+}
+
+
+void liftPresets() {
+	if (presetX.isPressed()) {
+		liftPosition(330, 50);
+	}
+	if (presetA.isPressed()) {
+		liftPosition(240, 50);
+	}
+	if (presetB.isPressed()) {
+		liftPosition(0, 50);
 	}
 }
 
@@ -87,6 +159,9 @@ void opcontrol() {
 		);
 
 		clampControl();
+		liftControl();
+		liftPresets();
+
 
 		pros::delay(20);
 	}
